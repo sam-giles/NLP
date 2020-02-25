847defd7bed6c0c2a17b90a06f66a3d96fd27cb4
samgiles <samdotgiles@gmail.com>
2020-02-25 14:06:57 -0600

    Pushing final changes

--- Program1/CorpusReader_TFIDF.py
+++ Program1/CorpusReader_TFIDF.py
@@ -2,35 +2,86 @@ import math
 import nltk
 from nltk.corpus import stopwords
 from nltk import stem
+from string import punctuation
+import numpy as np
+import re
 
-#Corpus Reader class
+sw = stopwords.words('english') + list(punctuation) + list("''")
+stemmer = stem.PorterStemmer()
+
+##CORPUS READER CLASS
 class CorpusReader_TFIDF:
 
-    #constructor object
-    def __init__(self, corpus, tf='raw', idf='base', stopword='default', stemmer=stem.PorterStemmer, ignorecase='yes'):
+    ##CONSTRUCTOR OBJECT
+    def __init__(self, corpus, tf='raw', idf='base', stopword=sw, stemmer=stemmer, ignorecase='yes'):
         self.corpus = corpus
-        self.corp_fileNames = corpus.fileids()
-        self.processedWords = self.corpus.words()
+        self.corp_fileids = corpus.fileids()
         self.tf = tf
         self.idf = idf
         self.stopword = stopword
         self.stemmer = stemmer
         self.ignorecase = ignorecase
 
+        #start data processing
+        #fileWordCounts represents a list of dictionaries of term occurences for each doc
+        #fileWordCounts = {}
+        #create vocabulary dict for all words in corpus
+        vocabulary = set()
+        for fileid in self.corp_fileids:
+            #fileWordCounts[fileid] = []
+            wordList = self.corpus.words(fileid)
+            #wordCount = {}
+            #preprocessing vocabulary
+            wordList = self.processData(wordList)
+            vocabulary.update(wordList)
+            #for word in wordList:
+            #    if word not in wordCount:
+            #        wordCount[word] = 1
+            #    else:
+            #        wordCount[word] = wordCount[word] + 1
+            #fileWordCounts[fileid].append(wordCount)
+
+        vocabulary = list(vocabulary)
+        word_index = {word: index for index, word in enumerate(vocabulary)}
+        
+        vocab_size = len(vocabulary)
+        docs_count = len(self.corp_fileids)
+
+        #calculate idf in constructor because that is the same
+        #for the entire corpus
+        word_idf = np.zeros(vocab_size)
+        for fileid in self.corp_fileids:
+            wordList = set(self.processData(self.corpus.words(fileid)))
+            indexes = [word_index[word] for word in wordList]
+            word_idf[indexes] += 1.0
+        
+        if self.idf is 'smooth':
+            word_idf = np.log(docs_count / (1 + word_idf).astype(float))
+        else:
+            word_idf = np.log(docs_count / (word_idf).astype(float))
+
+        #save important data as part of class instance
+        #self.fileWordCounts = fileWordCounts
+        self.vocabulary = vocabulary
+        self.word_index = word_index
+        self.word_idf = word_idf
+
+    ##HELPER FUNCTIONS
+
     #returns names of the documents in the corpus
     def fileids(self):
-        return self.corp_fileNames
+        return self.corp_fileids
 
     #returns raw text within the corpus
     def raw(self, fileids=None):
         if fileids is None:
-            fileids = self.corp_fileNames
+            fileids = self.corp_fileids
         return self.corpus.raw(fileids)
 
     #returns the words in list format of the corpus
     def words(self, fileids=None):
         if fileids is None:
-            fileids = self.corp_fileNames
+            fileids = self.corp_fileids
         return self.corpus.words(fileids)
 
     #opens the specified input in the corpus
@@ -41,147 +92,187 @@ class CorpusReader_TFIDF:
     def abspath(self, fileid):
         return self.corpus.abspath(fileid)
 
+    ##END HELPER FUNCTIONS
+
+    ##PROGRAM FUNCTIONS
+
     #return the tf-idf vector corresponding to the input
     def tf_idf(self, fileid=None, filelist=None):
         #first, figure out what data we are using
         if fileid is None:
             if filelist is None:
-                filelist = self.corp_fileNames
+                filelist = self.corp_fileids
             #else, filelist already exists as a parameter
         else:
             filelist = [fileid]
-        self.processedWords = self.words(filelist)
-        #then, handle any preprocessing
-        self.processedWords = self.processData(self.stopword, self.stemmer, self.ignorecase, self.processedWords)
-        #then, calculate the tf
-        tf = self.calculateTF(self.tf, filelist)
-        #then, calculate the idf
-        idf = self.calculateIDF(self.idf, filelist)
-        #now we have tf and idf, so we just have to calculate tfidf
-        tfidf = [[]]
-        for index, termVector in enumerate(tf):
-            tfidf.append([])
-            tfidf[index].append(termVector[0])
-            for i in range(len(termVector)):
-                #skip the first index, since that's the word itself
-                if i > 0:
-                    #tfidf = tf * idf
-                    tfidf[index].append((termVector[i])*(idf[index][1]))
+        #vector will be {filename: [{word: score}, {word: score}, etc.]}
+        vector = {}
+        for fileid in filelist:
+            vector[fileid] = {}
+            doc = self.processData(self.corpus.words(fileid))
+            #tfidf = tf*idf
+            for word in doc:
+                vector[fileid][word] = self.wordTF(doc, word) * self.word_idf[self.word_index[word]]
+        return vector
 
-        return tfidf
+    #return the list of the words in the corpus
+    def tf_idf_dim(self):
+        self.vocabulary.sort()
+        return self.vocabulary
+
+    #return a vector corresponding to the tf_idf vector for the new document
+    def tf_idf_new(self, words):
+        #use same idf from original corpus
+        words = self.processData(words)
+        vector = {}
+        for word in words:
+            if word in self.vocabulary:
+                vector[word] = self.wordTF(words, word) * self.word_idf[self.word_index[word]]
+        return vector
+
+    #return the cosine similarity between two documents in the corpus
+    def cosine_sim(self, fileids):
+        #first we need the tfidf vector for each document
+        tfidf0 = self.tf_idf(fileid=fileids[0])
+        tfidf1 = self.tf_idf(fileid=fileids[1])
+        #trim out the keys, keep the values
+        tfidf0, tfidf1 = tfidf0[fileids[0]], tfidf1[fileids[1]]
+        list_0 = list(tfidf0.values())
+        list_1 = list(tfidf1.values())
+
+        # convert to numpy arrays
+        a = np.array(list_0)
+        b = np.array(list_1)
+
+        #make sure the arrays are the same size
+        if a.size > b.size:
+            b.resize(a.size)
+        if b.size > a.size:
+            a.resize(b.size)
+        
+        #do l2 norm
+        l2a, l2b = 0.0, 0.0
+
+        #calculate l2a
+        for num in a:
+            num *= num
+            l2a += num
+        l2a = math.sqrt(l2a)
+
+        #calculate l2b
+        for num in b:
+            num *= num
+            l2b += num
+        l2b = math.sqrt(l2b)
+
+        #adjust the arrays
+        a = np.divide(a, l2a)
+        b = np.divide(b, l2b)
+
+        #get cosine similarity
+        dp = np.dot(a, b)
+        norm_a = np.linalg.norm(a)
+        norm_b = np.linalg.norm(b)
+        cosineSim = dp / (norm_a * norm_b)
+
+        return cosineSim
+
+
+    #Theis function returns the cosine similarity between 
+    # fileid and the new document specified by the [words] list
+    def cosine_sim_new(self, words, fileid):
+        #get tfidf of words and fileid
+        #tf_idf_new preprocesses words so we don't have to do that here
+        words_tfidf = self.tf_idf_new(words)
+        file_tfidf = self.tf_idf(fileid=fileid)
+
+        #trim out the key, keep the values
+        file_tfidf = file_tfidf[fileid]
+
+        list_words = list(words_tfidf.values())
+        list_file = list(file_tfidf.values())
+
+        # convert to numpy arrays
+        a = np.array(list_words)
+        b = np.array(list_file)
+
+        #make sure the arrays are the same size
+        if a.size > b.size:
+            b.resize(a.size)
+        if b.size > a.size:
+            a.resize(b.size)
+        
+        #do l2 norm
+        l2a, l2b = 0.0, 0.0
+
+        #calculate l2a
+        for num in a:
+            num *= num
+            l2a += num
+        l2a = math.sqrt(l2a)
+
+        #calculate l2b
+        for num in b:
+            num *= num
+            l2b += num
+        l2b = math.sqrt(l2b)
+
+        #adjust the arrays
+        a = np.divide(a, l2a)
+        b = np.divide(b, l2b)
+
+        #get cosine similarity
+        dp = np.dot(a, b)
+        norm_a = np.linalg.norm(a)
+        norm_b = np.linalg.norm(b)
+        cosineSim = dp / (norm_a * norm_b)
+
+        return cosineSim
+
+    ##END PROGRAM FUNCTIONS
+
+    ##MY OWN FUNCTIONS
 
     #my own function called to preprocess the data
-    def processData(self, stopword, stemmer, ignorecase, data):
+    #returns the data after it is processed
+    def processData(self, data):
         #now we handle preprocessing
-        #handle stemming
-        data = [stemmer.stem(word) for word in data]
-
-        #if we are ignoring case, make all the words lowercase
-        if ignorecase is not 'no':
-            data = [word.lower() for word in data]
-
-        #handle stopwords
-        if stopword is not 'none':
-            sw = []
-            if stopword is 'default':
-                sw = set(stopwords.words('english'))
-            else:
-                with open(stopword, 'r') as f:
+        #check ignorecase
+        if self.ignorecase is not 'no':
+            data = [w.lower() for w in data]
+        #stem before stopwords
+        data = [self.stemmer.stem(w) for w in data]
+        #check if stemwords are passed in as a file
+        sw = []
+        if self.stopword != 'none':
+            if isinstance(self.stopword, list):
+                sw = self.stopword
+            else: #it's going to be a filepath
+                with open(self.stopword,'r') as f:
                     for line in f:
                         for word in line.split():
                             sw.append(word)
-                sw = set(sw)
-            #if we are ignoring case, we also need to .lower() the stopwords
-            if ignorecase is not 'no':
-                sw = [word.lower() for word in sw]
-            data = [word for word in data if not word in sw]
-
-        #lastly we need to sort the words in alphabetical order and remove duplicates
-        data = list(set(data))
-        data.sort()
+        data = [w for w in data if w not in sw and not w.isdigit()]
+        data = [w for w in data if(re.match("^[A-Za-z']*$", w))]
+        data = [w.replace("'","") for w in data]
+        data = [w for w in data if len(w)>0]
         return data
-    
-    #my own function, calculates the tf vector and is called in tf_idf
-    def calculateTF(self, tf, filelist):
-        vector=[[]]
-        docs = []
-        fileWordList = [[]]
-        wordList = []
-        #separate out each document that we are using
-        for filename in filelist:
-            fileWordList = self.words(filename)
-            fileWordList = self.processData(self.stopword, self.stemmer, self.ignorecase, fileWordList)
-            docs.append(fileWordList)
-        for doc in docs:
-            for word in doc:
-                wordList.append(word)
-        for index, word in enumerate(wordList):
-            vector.append([])
-            #each word is the first index of its list
-            vector[index].append(word)
-            #now calculate term frequency for each document
-            for doc in docs:
-                termCount = 0
-                for doc_term in doc:
-                    print(doc_term, word)
-                    if doc_term==word:
-                        termCount = termCount+1
-                        print(doc_term, word, termCount)
-                if tf is 'log':
-                    if termCount > 0:
-                        termCount = 1 + math.log2(termCount)
-                elif tf is 'binary':
-                    if termCount > 0:
-                        termCount = 1
-                    
-                vector[index].append(termCount)
 
-        vector = vector[:-1]     
-        return vector
-
-    #my own function, calculates the idf vector and is called in tf_idf
-    def calculateIDF(self, idf, filelist):
-        vector=[[]]
-        docs = []
-        numDocs = len(filelist)
-        #separate out each document that we are using
-        for filename in filelist:
-            wordList = self.words(filename)
-            wordList = self.processData(self.stopword, self.stemmer, self.ignorecase, wordList)
-            docs.append(wordList)
-        for index, word in enumerate(self.processedWords):
-            vector.append([])
-            #each word is the first index of its list
-            vector[index].append(word)
-            n = 0
-            for doc in docs:
-                if word in doc:
-                    n = n+1
-            #handle smooth vs base
-            if idf is 'smooth':
-                idf = math.log2(1+(numDocs/n))
+    #calculates the tf of a given word in a given document
+    def wordTF(self, document, word):
+        if self.tf is 'raw':
+            return float(document.count(word)) / len(document)
+        elif self.tf is 'log':
+            return float(1 + math.log2(document.count(word)) / len(document))
+        else: #self.tf is binary
+            if document.count(word) > 0:
+                return float(1)
             else:
-                idf = math.log2(numDocs/n)
-            vector[index].append(idf)
-        vector = vector[:-1]
-        return vector
+                return float(0)
 
-    #return the list of the words in the order of the dimension
-    # of each corresponding to each vector of the tf-idf vector
-    ##TODO: def tf_idf_dim(self):
+    ##END MY FUNCTIONS
 
-    #return a vector corresponding to the tf_idf vector for the new document
-    ##TODO: def tf_idf_new(self, words):
-
-    #return thecosine similarity between two documents in the corpus
-    def cosine_sim(self, fileid):
-        tf1 = self.calculateTF(self.tf, [fileid[0]])
-        tf2 = self.calculateTF(self.tf, [fileid[1]])
-        print(tf2)
-
-    #The function return the cosine similarity between 
-    # fileid and the new document specify by the [words] list
-    ##TODO: def cosine_sim_new(self, words, fileid):
+##END CORPUS READER CLASS
     
 
     
--- Program1/Program1.py
+++ Program1/Program1.py
@@ -4,16 +4,85 @@ import nltk.corpus
 from nltk import stem
 import time
 
+#start timer
 start_time = time.time()
-ps = stem.PorterStemmer()
 
 brown = nltk.corpus.brown
 stateOfTheUnion = nltk.corpus.state_union
 
-brownReader = CorpusReader_TFIDF(brown, tf='raw', idf='base', stopword='default', stemmer=ps, ignorecase='yes')
-print(brownReader.cosine_sim(['ca03', 'ca04']))
+##OUTPUT FOR BROWN
+brownReader = CorpusReader_TFIDF(corpus=brown)
+print('-----------------------------')
+print('Brown Corpus')
+print('--------------------')
+print('First 15 words of tf_idf_dim')
+brown_first15 = brownReader.tf_idf_dim()[:15]
+#print the first 15 words of the vector
+print(brown_first15)
+print('--------------------')
+brown_fileids = brownReader.fileids()[:5]
+brown_tfidf = brownReader.tf_idf()
+#print the scores of those 15 words for the first 5 docs in the corpus
+for fileid in brown_fileids:
+    print(fileid, end = ' ')
+    #tfidf = brownReader.tf_idf(fileid=fileid)
+    for word in brown_first15:
+        if word not in brown_tfidf[fileid].keys():
+            print(0, end = ' ')
+        else:
+            print(brown_tfidf[fileid][word], end = ' ')
+    print()
+print('--------------------')
+#print cosine similarities
+print('ca01 ca02', brownReader.cosine_sim(['ca01', 'ca02']))
+print('ca01 ca03', brownReader.cosine_sim(['ca01', 'ca03']))
+print('ca01 ca04', brownReader.cosine_sim(['ca01', 'ca04']))
+print('ca01 ca05', brownReader.cosine_sim(['ca01', 'ca05']))
+print('ca02 cb01', brownReader.cosine_sim(['ca02', 'cb01']))
+print('ca02 cb02', brownReader.cosine_sim(['ca02', 'cb02']))
+print('ca02 cb03', brownReader.cosine_sim(['ca02', 'cb03']))
+print('ca02 cb04', brownReader.cosine_sim(['ca02', 'cb04']))
+print('ca02 cb05', brownReader.cosine_sim(['ca02', 'cb05']))
+print('ca02 ca02', brownReader.cosine_sim(['ca02', 'ca02']))
 
-# stateReader = CorpusReader_TFIDF(stateOfTheUnion, 'tf', 'idf')
-# print(stateReader.fileids())
+##END OUTPUT FOR BROWN
+
+##OUTPUT FOR STATE OF THE UNION
+
+stateReader = CorpusReader_TFIDF(corpus=stateOfTheUnion)
+print('-----------------------------')
+print('State of the Union Corpus')
+print('--------------------')
+print('First 15 words of tf_idf_dim')
+state_first15 = stateReader.tf_idf_dim()[:15]
+#print the first 15 words of the vector
+print(state_first15)
+print('--------------------')
+state_fileids = stateReader.fileids()[:5]
+state_tfidf = stateReader.tf_idf()
+#print the scores of those 15 words for the first 5 docs in the corpus
+for fileid in state_fileids:
+    print(fileid, end = ' ')
+    #tfidf = brownReader.tf_idf(fileid=fileid)
+    for word in state_first15:
+        if word not in state_tfidf[fileid].keys():
+            print(0, end = ' ')
+        else:
+            print(state_tfidf[fileid][word], end = ' ')
+    print()
+print('--------------------')
+#print cosine similarities
+print('1945-Truman 1946-Truman', stateReader.cosine_sim(['1945-Truman.txt', '1946-Truman.txt']))
+print('1945-Truman 1947-Truman', stateReader.cosine_sim(['1945-Truman.txt', '1947-Truman.txt']))
+print('1945-Truman 1948-Truman', stateReader.cosine_sim(['1945-Truman.txt', '1948-Truman.txt']))
+print('1945-Truman 1949-Truman', stateReader.cosine_sim(['1945-Truman.txt', '1949-Truman.txt']))
+print('1945-Truman 1950-Truman', stateReader.cosine_sim(['1945-Truman.txt', '1950-Truman.txt']))
+print('2003-GWBush 2004-GWBush', stateReader.cosine_sim(['2003-GWBush.txt', '2004-GWBush.txt']))
+print('2003-GWBush 2005-GWBush', stateReader.cosine_sim(['2003-GWBush.txt', '2005-GWBush.txt']))
+print('2003-GWBush 2006-GWBush', stateReader.cosine_sim(['2003-GWBush.txt', '2006-GWBush.txt']))
+print('2002-GWBush 2006-GWBush', stateReader.cosine_sim(['2002-GWBush.txt', '2006-GWBush.txt']))
+print('2003-GWBush 2003-GWBush', stateReader.cosine_sim(['2003-GWBush.txt', '2003-GWBush.txt']))
+
+##END OUTPUT FOR STATE OF THE UNION
 
 print("---Program took %s seconds ---" % (time.time() - start_time))
